4da109d67cb7d886113ee6e64b6a93776b7d04a5
chaechae001 <287885670+chaechae001@users.noreply.github.com>
2026-08-26 16:21:30 +0900

    02_rtl 사칙연산 테스트 완료

---
+++ next_js/02_rtl/src/_tests_/calcModule.test.js
@@ -0,0 +1,106 @@
+// test()       : 특정한 테스트 단위 의미
+// expect()    : 테스트 실행 의미
+// describe()  : test()의 group, describe는 describe를 담을 수 있다.
+
+// describe('묶음에 대한 설명', 함수)
+import {divide, minus, multiply, plus} from "@/app/calcModule";
+import {render, screen} from "@testing-library/react";
+import App from "@/app/page";
+import {userEvent} from "@testing-library/user-event/dist/cjs/setup/index.js";
+
+describe('사칙연산 UI 테스트', function(){
+
+    test('더하기 테스트', async function(){
+        // 1. UI 가져옴
+            // 테스트할 UI는 App에 있으므로 App에서 UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+
+        // 3. 특정 이벤트 발생 시
+        await userEvent.type(su1,'10');
+        await userEvent.type(su2,'20');
+        await userEvent.selectOptions(oper,'+');
+        await userEvent.click(btn);
+
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 30');
+    });
+
+    test('빼기 테스트', async function(){
+        // 1. UI 가져옴
+        // 테스트할 UI는 App에 있으므로 App에서 UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+
+        // 3. 특정 이벤트 발생 시
+        await userEvent.type(su1,'30');
+        await userEvent.type(su2,'20');
+        await userEvent.selectOptions(oper,'-');
+        await userEvent.click(btn);
+
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 10');
+    });
+
+    test('곱하기 테스트', async function(){
+        // 1. UI 가져옴
+        // 테스트할 UI는 App에 있으므로 App에서 UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+
+        // 3. 특정 이벤트 발생 시
+        await userEvent.type(su1,'30');
+        await userEvent.type(su2,'100');
+        await userEvent.selectOptions(oper,'*');
+        await userEvent.click(btn);
+
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 300');
+    });
+
+    test('나누기 테스트', async function(){
+        // 1. UI 가져옴
+        // 테스트할 UI는 App에 있으므로 App에서 UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+
+        // 3. 특정 이벤트 발생 시
+        await userEvent.type(su1,'4');
+        await userEvent.type(su2,'2');
+        await userEvent.selectOptions(oper,'/');
+        await userEvent.click(btn);
+
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 2');
+    });
+
+
+});
+
+/*
+toBe() : 숫자, 문자, 불리언 타입의 값에 일치
+toEqual()  : 객체나 배열의 일치
+toContain() : 배열이나 문자열 내에 특정 값 포함 여부
+toMatch() : 문자열이 지정된 정규표현식 패턴에 일치하는 지
+toThrow() : 특정 에러가 발생하는 지 여부
+ */
