015f56537596b02d9a69b218cf5f5992fd033f7f
chaechae001 <287885670+chaechae001@users.noreply.github.com>
2026-09-12 17:40:25 +0900

    이미지 분류

---
+++ langchain/06_image_class/image_service.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import traceback
+
+from transformers import pipeline
+
+# window 경고메시지 지우기(선택사항)
+os.environ["HF_HUB_DISABLE_SYMLINK_WARNING"] = "1"
+
+# 이미지 분류
+vision = pipeline(model="google/vit-base-patch16-224")
+
+def fileUpload(file,path):
+    success = -1
+    try:
+        with open(path,'wb') as file_obj:
+            shutil.copyfileobj(file,file_obj)
+        success = 1
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+    return success
+
+def class_img(img_path):
+    result = vision(img_path)
+    print(result)
+    if os.path.exists(img_path):
+        os.remove(img_path)
+
+    return result[0]
